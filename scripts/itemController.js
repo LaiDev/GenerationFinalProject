2779da77add2c585194a2f97c9cd98f94962332f
Aalaizha B <l.bnjamin0309@gmail.com>
2023-04-28 10:07:56 -0400

    ADD: PostController class

--- scripts/itemController.js
+++ scripts/itemController.js
@@ -1,6 +1,12 @@
 //Create an Class called  PostsControler
 //PostController will be responsible for managing  posts in the applications
 
+class PostController{
+    constructor(){
+        this.items = [];
+    }
+}
+
 //In the contructor make a prperty called items that is equal to an empty array
 
 //In the PostController constructor add a paramater called currentId. Give it default value of 0
@@ -8,3 +14,5 @@
 //Create a method called addItem
 //addItem should accept all the info needed to create a post. (Ex. title, description, Img) 
 //In addItem, increment the currentId by 1
+
+
